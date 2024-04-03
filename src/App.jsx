3a6bf478973e9ba57e9494299c3f031310fd475f
Chetan Sharma <ch9412233@gmail.com>
2024-04-03 19:12:48 +0530

    Appwrite setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <div className="text-center font-bold">
+        
+    </div>
+  )
+}
+
+export default App
